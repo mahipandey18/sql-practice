508b8464b82a804d31657e8010310fb1bb0f6e66
Mahi Pandey <mahipandey1813@gmail.com>
2025-12-17 00:33:52 +0530

    Create SQL file for leetcode 1757

--- basic/leetcode_1757.sql
+++ basic/leetcode_1757.sql
@@ -3,6 +3,6 @@
 -- learning: enum columns behave like strings and require quotes
 
 --solution
-select product_id 
-from Products 
-where low_fats = 'Y' and recyclable = 'Y';
+SELECT product_id 
+FROM Products 
+WHERE low_fats = 'Y' AND recyclable = 'Y';
