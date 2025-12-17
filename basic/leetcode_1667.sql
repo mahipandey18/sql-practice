fbfdba9c33e6b9f3cad62bb0fdff32baed765b03
Mahi Pandey <mahipandey1813@gmail.com>
2025-12-18 00:19:05 +0530

    Delete basic/leetcode_1667.sql

--- basic/leetcode_1667.sql
+++
@@ -1,8 +0,0 @@
--- Concept: capitalize first letter of a column
--- Approach: extract the first character, convert it to upper case, and concatenate it with the remaining substring
--- Learning: string functions like upper, lower, substring, and concat can be combined to format varchar values
-
---solution
-SELECT user_id, concat(upper(left(name,1)), lower(substring(name,2))) AS name
-FROM Users
-ORDER BY user_id;
