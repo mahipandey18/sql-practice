9e71e62ef3bd17b3aaaa10f32ccd13051ffed68e
Mahi Pandey <mahipandey1813@gmail.com>
2025-12-18 00:21:44 +0530

    Delete basic/leetcode_595.sql

--- basic/leetcode_595.sql
+++
@@ -1,8 +0,0 @@
--- leetcode 595: big countries
--- approach: filter rows using where clause with area and population conditions
--- learning: use logical or to combine multiple filtering criteria
-
---solution
-SELECT name, population, area
-FROM World
-WHERE population >=25000000 OR area>=3000000;
