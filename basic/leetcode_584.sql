35a75e5c97bc6bc2f389aaab07c8e3b0600478af
Mahi Pandey <mahipandey1813@gmail.com>
2025-12-18 00:21:10 +0530

    Delete basic/leetcode_584.sql

--- basic/leetcode_584.sql
+++
@@ -1,8 +0,0 @@
--- leetcode 584: find customer referee
--- approach: use where clause with null check and not equal condition
--- learning: null values are not included in inequality comparisons and must be handled explicitly
-
---solution
-SELECT name 
-FROM Customer
-WHERE referee_id IS NULL OR referee_id != 2;
