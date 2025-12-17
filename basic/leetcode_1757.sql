570fadbcb7b6d0207fd400641d664ea137046167
Mahi Pandey <mahipandey1813@gmail.com>
2025-12-18 00:20:37 +0530

    Delete basic/leetcode_1757.sql

--- basic/leetcode_1757.sql
+++
@@ -1,8 +0,0 @@
--- leetcode 1757: recyclable and low fat products
--- approach: use where condition to check enum values
--- learning: enum columns behave like strings and require quotes
-
---solution
-SELECT product_id 
-FROM Products 
-WHERE low_fats = 'Y' AND recyclable = 'Y';
