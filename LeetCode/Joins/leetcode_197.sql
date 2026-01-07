c54c7efc7a6b6dbd279a6f0487bbc178519a66bf
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-07 14:24:27 +0530

    Update leetcode_197.sql

--- LeetCode/Joins/leetcode_197.sql
+++ LeetCode/Joins/leetcode_197.sql
@@ -1,1 +1,9 @@
+-- LeetCode 197: Rising Temperature
+-- Approach: self-join weather records on consecutive dates and compare temperatures
+-- Learning: self joins help compare rows across different time periods
 
+--solution
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
