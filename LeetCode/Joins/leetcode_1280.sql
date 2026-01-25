1704242a4b3048a63374b5ee26747c9733789bf3
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-25 15:24:20 +0530

    Update leetcode_1280.sql

--- LeetCode/Joins/leetcode_1280.sql
+++ LeetCode/Joins/leetcode_1280.sql
@@ -1,1 +1,6 @@
+-- LeetCode 1280: Students and Examinations
+-- Approach: use cross join to generate all student-subject combinations, then left join exams
+-- Learning: cross join is required to create missing combinations, which left join alone cannot do
+
+--Solution
 
