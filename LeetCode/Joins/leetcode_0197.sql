e5bc92e9d77e6bda411a4eb4c38364fff74a5a10
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-25 14:41:51 +0530

    Update leetcode_0197.sql

--- LeetCode/Joins/leetcode_0197.sql
+++ LeetCode/Joins/leetcode_0197.sql
@@ -2,7 +2,7 @@
 -- Approach: self-join weather records on consecutive dates and compare temperatures
 -- Learning: self joins help compare rows across different time periods
 
---solution
+--Solution
 SELECT w1.id
 FROM Weather w1
 JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
