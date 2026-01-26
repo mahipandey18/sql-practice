6f5a3eda2a2aae52a068ffcf55f4ebf33d3be9f3
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 17:32:23 +0530

    Update leetcode_0177.sql

--- LeetCode/Sub-Queries/leetcode_0177.sql
+++ LeetCode/Sub-Queries/leetcode_0177.sql
@@ -1,1 +1,5 @@
+-- LeetCode 177: Nth Highest Salary
+-- Approach: for each distinct salary, count how many salaries are higher and match N-1
+-- Learning: rank can be derived by counting greater values using correlated subqueries
 
+--Solution
