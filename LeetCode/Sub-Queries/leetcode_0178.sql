ed0a68a039a0ef6e803d61e4fc42108e3eddb930
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 18:29:06 +0530

    Update leetcode_0178.sql

--- LeetCode/Sub-Queries/leetcode_0178.sql
+++ LeetCode/Sub-Queries/leetcode_0178.sql
@@ -1,1 +1,7 @@
+-- LeetCode 178: Rank Scores
+-- Approach: use DENSE_RANK() window function to assign ranks based on score in descending order
+-- Learning: DENSE_RANK gives the same rank for equal scores without skipping rank numbers
 
+--Solution
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
+FROM Scores;
