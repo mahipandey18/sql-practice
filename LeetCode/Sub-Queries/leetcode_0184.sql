e25006eb6d2c5186c508cfedf1946578ae2f63be
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 16:29:31 +0530

    Update leetcode_0184.sql

--- LeetCode/Sub-Queries/leetcode_0184.sql
+++ LeetCode/Sub-Queries/leetcode_0184.sql
@@ -1,1 +1,13 @@
+-- LeetCode 184: Department Highest Salary
+-- Approach: use a correlated subquery to find the maximum salary per department
+-- Learning: correlated subqueries allow row-wise comparisons within grouped categories
 
+--Solution
+SELECT d.name as Department, e.name as Employee, e.salary as Salary 
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
