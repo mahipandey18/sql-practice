d6ec7a2cea8427413efbdf3a6d2c70d1f6b6ed1a
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 18:45:38 +0530

    Update leetcode_0185.sql

--- LeetCode/Sub-Queries/leetcode_0185.sql
+++ LeetCode/Sub-Queries/leetcode_0185.sql
@@ -1,1 +1,6 @@
+-- LeetCode 185: Department Top Three Salaries
+-- Approach: use a correlated subquery to count higher distinct salaries within each department
+-- Learning: top-N per group can be solved by filtering rows where higher distinct values are less than N
+
+--Solution
 
