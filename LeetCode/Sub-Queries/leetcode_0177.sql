4df829f32b81f0d7416adb2b56c58c540569a4b4
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 17:36:41 +0530

    Update leetcode_0177.sql

--- LeetCode/Sub-Queries/leetcode_0177.sql
+++ LeetCode/Sub-Queries/leetcode_0177.sql
@@ -3,3 +3,15 @@
 -- Learning: rank can be derived by counting greater values using correlated subqueries
 
 --Solution
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    SELECT distinct salary
+    FROM Employee e1
+    WHERE (
+        SELECT COUNT(distinct salary)
+        FROM Employee e2
+        WHERE e2.salary > e1.salary
+    ) = N - 1
+  );
+END
