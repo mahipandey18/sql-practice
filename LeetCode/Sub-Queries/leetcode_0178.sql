03bdf9f6aa0f9590a27ca5c5cc1b1f4e82713470
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-26 18:51:52 +0530

    Update leetcode_0178.sql

--- LeetCode/Sub-Queries/leetcode_0178.sql
+++ LeetCode/Sub-Queries/leetcode_0178.sql
@@ -3,5 +3,12 @@
 -- Learning: DENSE_RANK gives the same rank for equal scores without skipping rank numbers
 
 --Solution
-SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
-FROM Scores;
+SELECT s1.score,
+       (
+           SELECT COUNT(DISTINCT s2.score)
+           FROM Scores s2
+           WHERE s2.score > s1.score
+       ) + 1 AS `rank`
+FROM Scores s1
+ORDER BY s1.score DESC;
+
