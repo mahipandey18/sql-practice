374c3b8b8a901672d35c9c577a7d05caa7a82a5f
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-28 00:43:22 +0530

    Update leetcode_0184.sql

--- LeetCode/Window-Functions/leetcode_0184.sql
+++ LeetCode/Window-Functions/leetcode_0184.sql
@@ -1,1 +1,10 @@
+--Solution
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM (
+    SELECT *,
+           DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+    FROM Employee
+) e
+JOIN Department d ON e.departmentId = d.id
+WHERE rnk = 1;
 
