b37c3611825a773ef4562b57c02c6b12c8f9712b
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-28 23:46:11 +0530

    Update leetcode_1204.sql

--- LeetCode/Window-Functions/leetcode_1204.sql
+++ LeetCode/Window-Functions/leetcode_1204.sql
@@ -1,1 +1,11 @@
 --Solution
+SELECT person_name
+FROM(
+    SELECT person_name, turn,
+    --calculate sum of each person's weight as per thier turn
+    SUM(weight) OVER (ORDER BY turn) as cum_weight 
+    FROM Queue
+) t
+WHERE cum_weight <=1000
+ORDER BY cum_weight desc
+LIMIT 1;
