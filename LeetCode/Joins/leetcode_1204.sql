a1e2b01cecf70eafea59bee850edf570f4a57342
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-28 23:40:40 +0530

    Update leetcode_1204.sql

--- LeetCode/Joins/leetcode_1204.sql
+++ LeetCode/Joins/leetcode_1204.sql
@@ -1,3 +1,7 @@
+-- LeetCode 1204: Maximum Total Weight of Queue
+-- Approach: Use a self-join to compute cumulative weight for each person in the queue, group by q1.turn to sum weights of all people ahead including themselves, filter groups where sum <= 1000, and pick the last person with maximum allowed weight
+-- Technique: Self-join, GROUP BY, HAVING, ORDER BY with LIMIT to find the last eligible person based on cumulative weight
+
 --Solution
 SELECT q1.person_name
 FROM Queue  q1
