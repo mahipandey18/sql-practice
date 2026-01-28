18d3147aa7df87afadb907e5a118e49d5d4573c0
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-28 23:40:29 +0530

    Update leetcode_1204.sql

--- LeetCode/Joins/leetcode_1204.sql
+++ LeetCode/Joins/leetcode_1204.sql
@@ -1,1 +1,9 @@
 --Solution
+SELECT q1.person_name
+FROM Queue  q1
+JOIN Queue q2 ON q1.turn >= q2.turn # getting all the people AHEAD of current q1 person
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <=1000
+ORDER BY SUM(q2.weight) DESC # pick with largest weight before the limit aka last one
+LIMIT 1;
+
