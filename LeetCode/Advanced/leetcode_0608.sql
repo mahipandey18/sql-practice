3e9271b493d790849fb60d9463a40af11c0b12f3
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-29 17:46:04 +0530

    Update leetcode_0608.sql

--- LeetCode/Advanced/leetcode_0608.sql
+++ LeetCode/Advanced/leetcode_0608.sql
@@ -1,1 +1,10 @@
 --Solution
+SELECT id,
+    CASE 
+        WHEN p_id IS NULL then "Root"
+        WHEN id IN (
+            SELECT DISTINCT p_id FROM tree
+            ) THEN "Inner"
+        ELSE "Leaf" 
+    END AS type
+FROM Tree;
