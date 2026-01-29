3f4c1121817f7c299dc5acb8bd72adae1e9ece73
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-29 18:00:12 +0530

    Update leetcode_0626.sql

--- LeetCode/Advanced/leetcode_0626.sql
+++ LeetCode/Advanced/leetcode_0626.sql
@@ -1,1 +1,12 @@
 --Solution
+SELECT
+    CASE
+        WHEN id % 2 = 1 
+        AND id+1 in (
+            SELECT id FROM Seat
+        ) THEN id + 1
+        WHEN id % 2 = 0 THEN  id-1
+        ELSE id
+    END as id, student
+FROM Seat
+ORDER BY id;
