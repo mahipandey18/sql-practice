3219da437a9e3ce4eb45bd61fb6447499e178871
Mahi Pandey <mahipandey1813@gmail.com>
2026-01-31 00:53:08 +0530

    Update leetcode_1164.sql

--- LeetCode/Advanced/leetcode_1164.sql
+++ LeetCode/Advanced/leetcode_1164.sql
@@ -1,2 +1,10 @@
 --Solution
-
+SELECT distinct p1.product_id, IFNULL(
+    (SELECT p2.new_price
+    FROM Products p2
+    WHERE p2.product_id = p1.product_id
+    AND p2.change_date <= '2019-08-16'
+    ORDER BY change_date desc
+    LIMIT 1), 10
+) as price
+FROM Products p1;
